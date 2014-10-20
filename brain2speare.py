b4232260dbc033cc077780ed7611ed600f5e9d84
Matthew Darby <me@mjdarby.net>
2014-10-20 21:51:40 +0100

    First steps towards making the scripts pretty. Should move wordlists into files instead

--- brain2speare.py
+++ brain2speare.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import re, sys, roman, math
+import re, sys, roman, math, random
+
+# Functions to convert the BF to Shakespeare
 
 def get_parentheses(text):
   stack = []
@@ -20,6 +22,7 @@ def bf_to_shakespeare(instructions):
   instr_pointer = 0
   scene_number = 2
   sums_pointer = 0
+  shakespeare = ""
 
   sums = group_sums(instructions)
   instructions = remove_groups(instructions)
@@ -30,53 +33,62 @@ def bf_to_shakespeare(instructions):
   while instr_pointer < len(instructions):
     instr = instructions[instr_pointer]
     if instr == '>':
-      forward_pointer()
+      shakespeare += forward_pointer()
     elif instr == '<':
-      backward_pointer()
+      shakespeare += backward_pointer()
     elif instr == '?':
-      modify_value(sums[sums_pointer])
+      shakespeare += modify_value(sums[sums_pointer])
       sums_pointer += 1
     elif instr == '.':
-      print_value()
+      shakespeare += print_value()
     elif instr == ',':
-      get_value()
+      shakespeare += get_value()
     elif instr == '[':
-      open_bracket(instr_pointer, parentheses)
+      shakespeare += open_bracket(instr_pointer, parentheses)
     elif instr == ']':
-      close_bracket(instr_pointer, parentheses)
+      shakespeare += close_bracket(instr_pointer, parentheses)
     instr_pointer += 1
 
-def print_shakespeare(text):
+  return shakespeare
+
+def get_shakespeare(text):
+  shakespeare = ""
   parentheses = {}
-  print("The Interpreted Brainfuck.\n")
-  print("Romeo, a stack that represents man's present and past.")
-  print("Juliet, a stack that represents woman's future.")
-  print("Lady Macbeth, a zero that is only good for comparison.\n")
-  print("Act I: An egregious abuse.")
-  print("Scene I: The fattening of Juliet.") # Build up the Juliet stack
-  print("[Enter Romeo and Juliet]\n")
-  sys.stdout.write("Romeo: ")
+  shakespeare += "The Interpreted Brainfuck.\n\n"
+  shakespeare += "Romeo, a stack that represents man's present and past.\n"
+  shakespeare += "Juliet, a stack that represents woman's future.\n"
+  shakespeare += "Lady Macbeth, a zero that is only good for comparison.\n\n"
+  shakespeare += "Act I: An egregious abuse.\n"
+  shakespeare += "Scene I: The fattening of Juliet.\n" # Build up the Juliet stack
+  shakespeare += "[Enter Romeo and Juliet]\n\n"
+  shakespeare += "Romeo: "
   for x in range(1024):
-    sys.stdout.write("Remember thyself! ")
-  sys.stdout.write("\n")
-  print("[Exeunt]\n")
-  print("Act II: Our main performance.\n")
-  print("Scene I: It begins here.\n")
-  print("[Enter Romeo and Juliet]\n")
-  bf_to_shakespeare(text)
-  print("\n[Exeunt]")
+    shakespeare += "Remember yourself! "
+  shakespeare += "\n"
+  shakespeare += "[Exeunt]\n\n"
+  shakespeare += "Act II: Our main performance.\n\n"
+  shakespeare += "Scene I: It begins here.\n\n"
+  shakespeare += "[Enter Romeo and Juliet]\n\n"
+  shakespeare += bf_to_shakespeare(text)
+  shakespeare += "\n[Exeunt]\n"
+  return shakespeare
 
 def forward_pointer():
-  print("Juliet: Remember thyself!")
-  print("Romeo: Recall the sins of the fathers.")
-  print("Juliet: You are as good as me.")
+  command = ""
+  command += "Juliet: Remember yourself!\n"
+  command += "Romeo: Recall the sins of the fathers.\n"
+  command += "Juliet: You are as good as me.\n"
+  return command
 
 def backward_pointer():
-  print("Romeo: You are as good as me! Remember thyself.")
-  print("Juliet: Recall yourself.")
+  command = ""
+  command += "Romeo: You are as good as me! Remember yourself.\n"
+  command += "Juliet: Recall yourself.\n"
+  return command
 
 def modify_value(value):
-  print(sum_up_or_subtract_down(value, value > 0))
+  command = sum_up_or_subtract_down(value, value > 0)
+  return command
 
 def sum_up_or_subtract_down(value, incrementing):
   command = "Juliet: "
@@ -111,7 +123,7 @@ def sum_up_or_subtract_down(value, incrementing):
       # If we're subtracting, it's easier to just subtract down
       for power_of_two in value_powers_of_two:
         command += generate_summation_statement(power_of_two, False)
-  return command
+  return command + "\n"
 
 def get_powers_of_two(value):
   binary_representation  = bin(value)[2:]
@@ -122,41 +134,57 @@ def get_powers_of_two(value):
   return powers_of_two
 
 def generate_summation_statement(power, incrementing):
-  command = "You are as large as the " + ("sum of " if incrementing else \
-"difference between ") + "yourself and a "
+  choices = [(incrementing, True), (not incrementing, False)]
+  positive_sum, positive_noun = random.choice(choices)
+  command = "Secondperson are as large as the " + ("sum of " if \
+positive_sum else "difference between ") + "yourself and a "
   for x in range(power):
-    command += "big "
-  command += "cat! "
+    if positive_noun:
+      command += "posadj "
+    else:
+      command += "negadj "
+  if positive_noun:
+    command += "cat! "
+  else:
+    command += "pig! "
   return command
 
 def open_bracket(pointer, parentheses):
+  command = ""
   open_paren, close_paren = parentheses[pointer]
-  print("\nScene " + numeral(open_paren) + ": Another scene.\n")
-  print("[Exeunt]\n")
-  print("[Enter Lady Macbeth and Romeo]\n")
-  print("Lady Macbeth: Are you as good as me?")
-  print("Romeo: If so, let us proceed to scene " + \
-        numeral(close_paren) + ".\n")
-  print("[Exit Lady Macbeth]\n")
-  print("[Enter Juliet]\n")
+  command += "\nScene " + numeral(open_paren) + ": Another scene.\n\n"
+  command += "[Exeunt]\n\n"
+  command += "[Enter Lady Macbeth and Romeo]\n\n"
+  command += "Lady Macbeth: Are secondperson as good as me?\n"
+  command += "Romeo: If so, let us proceed to scene " + \
+        numeral(close_paren) + ".\n\n"
+  command += "[Exit Lady Macbeth]\n\n"
+  command += "[Enter Juliet]\n\n"
+  return command
 
 def close_bracket(pointer, parentheses):
+  command = ""
   open_paren, close_paren = parentheses[pointer]
-  print("\n[Exit Juliet]\n")
-  print("[Enter Lady Macbeth]\n")
-  print("Lady Macbeth: Are you as bad as me?")
-  print("Romeo: If not, let us proceed to scene " + \
-        numeral(open_paren) + ".")
-  print("[Exeunt]\n")
-  print("Scene " + numeral(close_paren) + ": Another scene.\n")
-  print("[Exeunt]\n")
-  print("[Enter Romeo and Juliet]\n")
+  command += "\n[Exit Juliet]\n\n"
+  command += "[Enter Lady Macbeth]\n\n"
+  command += "Lady Macbeth: Are you as bad as me?\n"
+  command += "Romeo: If not, let us proceed to scene " + \
+        numeral(open_paren) + ".\n\n"
+  command += "[Exeunt]\n\n"
+  command += "Scene " + numeral(close_paren) + ": Another scene.\n\n"
+  command += "[Exeunt]\n\n"
+  command += "[Enter Romeo and Juliet]\n\n"
+  return command
 
 def print_value():
-  print("Juliet: Speak your mind!")
+  command = ""
+  command += "Juliet: Speak your mind!\n"
+  return command
 
 def get_value():
-  print("Juliet: Open your mind!")
+  command = ""
+  command += "Juliet: Open your mind!\n"
+  return command
 
 def numeral(number):
   return roman.toRoman(number)
@@ -177,6 +205,117 @@ def remove_groups(text):
   text = re.sub('\?+', '?', text)
   return text
 
+# Functions to spruce up the text a bit.
+
+def random_positive_adjective(arg):
+  return random.choice(['amazing',
+'beautiful',
+'blossoming',
+'bold',
+'brave',
+'charming',
+'clearest',
+'cunning',
+'cute',
+'delicious',
+'embroidered',
+'fair',
+'fine',
+'gentle',
+'golden',
+'good',
+'handsome',
+'happy',
+'healthy',
+'honest',
+'lovely',
+'loving',
+'mighty',
+'noble',
+'peaceful',
+'pretty',
+'prompt',
+'proud',
+'reddest',
+'rich',
+'smooth',
+'sunny',
+'sweet',
+'sweetest',
+'trustworthy',
+'warm'])
+
+def random_negative_adjective(arg):
+  return random.choice(['bad',
+'cowardly',
+'cursed',
+'damned',
+'dirty',
+'disgusting',
+'distasteful',
+'dusty',
+'evil',
+'fat',
+'fat-kidneyed',
+'fatherless',
+'foul',
+'hairy',
+'half-witted',
+'horrible',
+'horrid',
+'infected',
+'lying',
+'miserable',
+'misused',
+'oozing',
+'rotten',
+'rotten',
+'smelly',
+'snotty',
+'sorry',
+'stinking',
+'stuffed',
+'stupid',
+'vile',
+'villainous',
+'worried'])
+
+def random_positive_noun(arg):
+  return arg
+
+def random_negative_noun(arg):
+  return random.choice(['Thou', 'You'])
+
+def random_second_person(arg):
+  return random.choice(['Thou', 'You'])
+
+def random_second_person_lower(arg):
+  return random.choice(['thou', 'you'])
+
+def random_second_person_possessive(arg):
+  return random.choice(['thyself', 'yourself'])
+
+def replace_yous(text):
+  text = re.sub('secondperson', random_second_person_lower, text)
+  text = re.sub('Secondperson', random_second_person, text)
+  return text
+
+def replace_yourselfs(text):
+  return re.sub('yourself', random_second_person_possessive, text)
+
+def replace_adjectives(text):
+  text = re.sub('negadj', random_negative_adjective, text)
+  text = re.sub('posadj', random_positive_adjective, text)
+  return text
+
+def improve_shakespeare(text):
+  text = replace_yous(text)
+  text = replace_yourselfs(text)
+  text = replace_adjectives(text)
+  return text
+
+# Work the magic of translating and sprucing!
+
 if __name__ == "__main__":
   if len(sys.argv) < 2:
     print("Usage: ./brain2speare.py input.b > output.spl")
@@ -188,4 +327,4 @@ if __name__ == "__main__":
   text = re.sub('[^><\+\-\.,\[\]]', '', text)
   f.close()
 
-  print_shakespeare(text)
+  print(improve_shakespeare(get_shakespeare(text)))
